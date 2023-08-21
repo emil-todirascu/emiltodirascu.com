66e3f304d54a5db20f437415f86c990eef6687a8
Emil Todirascu <emil.todirascu@gmail.com>
2023-08-21 20:31:20 +0200

    hopefully fixed form submition

--- javascript/script.js
+++ javascript/script.js
@@ -29,3 +29,32 @@ function copyEmail() {
 		modal.close();
 	}, 2500);
 }
+
+document.getElementById("my-form").addEventListener("submit", function (event) {
+	event.preventDefault(); // Prevent the default form submission behavior
+
+	var formData = new FormData(event.target);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open(
+		"POST",
+		"https://formsubmit.co/b53dd580e275bfd553a5bd27fc8d42ce",
+		true
+	);
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				alert("Message sent!\nI will get back to you as soon as possible.");
+				event.target.reset();
+			} else {
+				alert(
+					"There was a problem sending the message. Please contact me directly through the links below or try again later."
+				);
+			}
+		}
+	};
+
+	// Send the form data
+	xhr.send(formData);
+});
